419481a718c694f33f8ddcbd39c02d168dcb716d
tjpaakkunainen <tomipaakkunainen@outlook.com>
2021-08-15 12:14:45 +0300

    added new component

--- palautusrepositorio/osa1/kurssitiedot/kurssitiedot/src/App.js
+++ palautusrepositorio/osa1/kurssitiedot/kurssitiedot/src/App.js
@@ -1,6 +1,71 @@
 import React from 'react'
 
 const Header = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.course}
+        </h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <h3>
+         {props.part}: {props.exers}
+      </h3>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+    <Part part={props.part1} exers={props.exercises1}/>
+    <Part part={props.part2} exers={props.exercises2}/>
+    <Part part={props.part3} exers={props.exercises3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises: {props.total}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App;
+
+/* 
+
+import React from 'react'
+
+const Header = (props) => {
   console.log(props)
   return (
     <div>
@@ -56,4 +121,4 @@ const App = () => {
   )
 }
 
-export default App;
+export default App; */
