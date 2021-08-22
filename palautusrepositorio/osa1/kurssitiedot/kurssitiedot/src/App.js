185fb9bce7dae91163be4498415d9d565deb1335
tjpaakkunainen <tomipaakkunainen@outlook.com>
2021-08-22 20:46:59 +0300

    nothing special

--- palautusrepositorio/osa1/kurssitiedot/kurssitiedot/src/App.js
+++ palautusrepositorio/osa1/kurssitiedot/kurssitiedot/src/App.js
@@ -10,23 +10,18 @@ const Header = (props) => {
   )
 }
 
-const Part = (props) => {
-  return (
-    <div>
-      <h3>
-         {props.part}: {props.exers}
-      </h3>
-    </div>
-  )
-}
-
 const Content = (props) => {
-  console.log(props)
   return (
     <div>
-    <Part part={props.part1} exers={props.exercises1}/>
-    <Part part={props.part2} exers={props.exercises2}/>
-    <Part part={props.part3} exers={props.exercises3}/>
+      <p>
+         {props.a}
+      </p>
+      <p>
+        {props.b}
+      </p>
+      <p>
+        {props.c}
+      </p>
     </div>
   )
 }
@@ -42,19 +37,28 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const course = {
+  name: 'Half Stack application development',
+  parts: [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
+  }
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Header course={course.name}/>
+      <Content a={course.parts[0].name + ' ' + course.parts[0].exercises} b={course.parts[1].name  + ' ' + course.parts[1].exercises} c={course.parts[2].name  + ' ' + course.parts[2].exercises}/>
+      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
     </div>
   )
 }
